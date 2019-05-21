d5b8a2425094970a992c82ecc63c88ed7322c0cd
Siddhant Vinchurkar <siddhantvinchurkar@gmail.com>
2019-05-21 16:43:29 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/* This is the main JS file for this web app */
+
+/* Main thread */
+window.onload = function () {
+    /* Initialize Firebase */
+    firebase.initializeApp({
+        apiKey: "AIzaSyB2R3EKvah0v7TXIJhmE-0Fyp0z0cqwUws",
+        authDomain: "tgd-experiment.firebaseapp.com",
+        databaseURL: "https://tgd-experiment.firebaseio.com",
+        projectId: "tgd-experiment",
+        storageBucket: "tgd-experiment.appspot.com",
+        messagingSenderId: "262835571788",
+        appId: "1:262835571788:web:014ae0235c5278c8"
+    });
+}
