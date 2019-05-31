7c037f07cf0b345aa3f6c5d55b357b1e94df170f
Siddhant Vinchurkar <siddhantvinchurkar@gmail.com>
2019-06-01 00:24:37 +0530

    Fixed sign in

--- main.js
+++ main.js
@@ -4,7 +4,89 @@
 var user = {};
 var signedIn = false;
 var db = '';
-var currentUserId = '';
+var currentUserDocId = '';
+var returningUser = false;
+var os = [
+    { name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+    { name: 'Windows', value: 'Win', version: 'NT' },
+    { name: 'iPhone', value: 'iPhone', version: 'OS' },
+    { name: 'iPad', value: 'iPad', version: 'OS' },
+    { name: 'Kindle', value: 'Silk', version: 'Silk' },
+    { name: 'Android', value: 'Android', version: 'Android' },
+    { name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+    { name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+    { name: 'Macintosh', value: 'Mac', version: 'OS X' },
+    { name: 'Linux', value: 'Linux', version: 'rv' },
+    { name: 'Palm', value: 'Palm', version: 'PalmOS' }
+];
+var browser = [
+    { name: 'Chrome', value: 'Chrome', version: 'Chrome' },
+    { name: 'Firefox', value: 'Firefox', version: 'Firefox' },
+    { name: 'Safari', value: 'Safari', version: 'Version' },
+    { name: 'Internet Explorer', value: 'MSIE', version: 'MSIE' },
+    { name: 'Opera', value: 'Opera', version: 'Opera' },
+    { name: 'BlackBerry', value: 'CLDC', version: 'CLDC' },
+    { name: 'Mozilla', value: 'Mozilla', version: 'Mozilla' }
+];
+var header = [
+    navigator.platform,
+    navigator.userAgent,
+    navigator.appVersion,
+    navigator.vendor,
+    window.opera
+];
+
+/* Identity RegEx Function */
+function matchItem(string, data) {
+	var i = 0,
+	j = 0,
+	html = '',
+	regex,
+	regexv,
+	match,
+	matches,
+	version;
+	for (i = 0; i < data.length; i += 1) {
+		regex = new RegExp(data[i].value, 'i');
+		match = regex.test(string);
+		if (match) {
+			regexv = new RegExp(data[i].version + '[- /:;]([\d._]+)', 'i');
+			matches = string.match(regexv);
+			version = '';
+				if(matches){
+					if (matches[1]){
+						matches = matches[1];
+					}
+				}
+			if (matches) {
+				matches = matches.split(/[._]+/);
+				for (j = 0; j < matches.length; j += 1) {
+					if(j === 0){
+						version += matches[j] + '.';
+					}
+					else{
+						version += matches[j];
+					}
+				}
+			}
+			else{
+				version = '0';
+			}
+			return {
+			name: data[i].name,
+			version: parseFloat(version)
+			};
+		}
+	}
+	return {name: 'unknown', version: 0};
+}
+
+/* Retrieve device identity */
+function getDeviceIdentity(){
+	agent = header.join(' ');
+	os = this.matchItem(agent, os);
+	browser = this.matchItem(agent, browser);
+}
 
 /* Set Text Function */
 function setText(line1, line2, line3){
@@ -47,20 +129,40 @@ function signIn(){
 
 	var provider = new firebase.auth.GoogleAuthProvider();
 	if(!signedIn){
+		getDeviceIdentity();
 		firebase.auth().signInWithPopup(provider).then(function(result){
 			user = result.user;
 			var tempCounter = 0;
 			db.collection('users').where('email', '==', user.email).get().then(function(querySnapshot){
 				querySnapshot.forEach((doc) => {
+					currentUserDocId = doc.id;
 					tempCounter++;
 				});
 				if(tempCounter < 1){
+					returningUser = false;
 					db.collection('users').add({
 					displayName: user.displayName,
 					email: user.email,
 					photoURL: user.photoURL
 					}).then(function(doc){
-						currentUserId = doc.id;
+						currentUserDocId = doc.id;
+					});
+					db.collection('users').doc(currentUserDocId).collection('activity_logs').add({
+						timestamp: new Date(),
+						os_name: os.name,
+						os_version: os.version,
+						browser_name: browser.name,
+						browser_version: browser.version
+					});
+				}
+				else{
+					returningUser = true;
+					db.collection('users').doc(currentUserDocId).collection('activity_logs').add({
+						timestamp: new Date(),
+						os_name: os.name,
+						os_version: os.version,
+						browser_name: browser.name,
+						browser_version: browser.version
 					});
 				}
 			});
