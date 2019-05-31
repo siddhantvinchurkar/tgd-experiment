0995b94b56d4e35c81c90662ba1af1fd969c68be
Siddhant Vinchurkar <siddhantvinchurkar@gmail.com>
2019-05-31 23:23:21 +0530

    Completed Introduction

--- main.js
+++ main.js
@@ -1,34 +1,141 @@
 /* This is the main JS file for this web app */
 
+/* Global Variables */
+var user = {};
+var signedIn = false;
+var db = '';
+var currentUserId = '';
+
+/* Set Text Function */
+function setText(line1, line2, line3){
+
+	document.getElementById('screen1').innerHTML = line1;
+	document.getElementById('screen2').innerHTML = line2;
+	document.getElementById('screen3').innerHTML = line3;
+	
+}
+
+/* Animate Introduction Function */
+function animateIntroduction(){
+
+	$("#screen1").fadeIn(1000);
+	setTimeout(function(){$("#screen2").fadeIn(1000);}, 1000);
+	setTimeout(function(){$("#screen3").fadeIn(1000);}, 2000);
+	setTimeout(function(){$("#screen1").fadeOut(1000);}, 6000);
+	setTimeout(function(){$("#screen2").fadeOut(1000);}, 7000);
+	setTimeout(function(){$("#screen3").fadeOut(1000);}, 8000);
+	
+}
+
+/* Animate Brief Function */
+function animateBrief(){
+
+	setText('I\'m omnipresent; and right now, most of my knowledge comes from people I interact with.', 'Use the mic button to talk to me.', 'I\'ll store your preferences if you sign in.');
+	$("#screen1").fadeIn(1000);
+	setTimeout(function(){$("#screen2").fadeIn(1000);}, 1000);
+	setTimeout(function(){$("#mic").fadeIn(1000);}, 10000);
+	setTimeout(function(){$("#screen3").fadeIn(1000);}, 6000);
+	setTimeout(function(){$("#sign_in_button").fadeIn(1000);}, 10000);
+	setTimeout(function(){$("#screen1").fadeOut(1000);}, 4000);
+	setTimeout(function(){$("#screen2").fadeOut(1000);}, 5000);
+	setTimeout(function(){$("#screen3").fadeOut(1000);}, 9000);
+	
+}
+
+/* Sign In Function */
+function signIn(){
+
+	var provider = new firebase.auth.GoogleAuthProvider();
+	if(!signedIn){
+		firebase.auth().signInWithPopup(provider).then(function(result){
+			user = result.user;
+			var tempCounter = 0;
+			db.collection('users').where('email', '==', user.email).get().then(function(querySnapshot){
+				querySnapshot.forEach((doc) => {
+					tempCounter++;
+				});
+				if(tempCounter < 1){
+					db.collection('users').add({
+					displayName: user.displayName,
+					email: user.email,
+					photoURL: user.photoURL
+					}).then(function(doc){
+						currentUserId = doc.id;
+					});
+				}
+			});
+			signedIn = true;
+			$("#sign_in_button").fadeOut(1000);
+			setTimeout(function(){
+			
+				document.getElementById('sign_in_button').innerHTML = '<i class="material-icons right">exit_to_app</i>Sign Out';
+				document.getElementById('username_profile').innerHTML = user.displayName + '<br />' + user.email;
+				document.getElementById('username_profile').href = user.photoURL;
+				
+			}, 1000);
+			setTimeout(function(){$("#sign_in_button").fadeIn(1000);}, 500);
+			setTimeout(function(){$("#username_profile").fadeIn(1000);}, 500);
+			}).catch(function(error){
+				signedIn = false;
+				console.log(error);
+			});
+	}
+	else{
+		firebase.auth().signOut().then(function() {
+			signedIn = false;
+			$("#sign_in_button").fadeOut(1000);
+			setTimeout(function(){document.getElementById('sign_in_button').innerHTML = '<i class="material-icons right">arrow_forward_ios</i>Sign In';}, 1000);
+			setTimeout(function(){$("#sign_in_button").fadeIn(1000);}, 500);
+			setTimeout(function(){$("#username_profile").fadeOut(1000);}, 500);
+		}).catch(function(error) {
+			console.log(error);
+		});
+	}
+	
+}
+
 /* Main Function */
 function main(){
 
-	/* Slowly fade the screen in */
-	$("#screen").fadeIn(2000);
+	/* Below is a storyline of the actions this app will perform */
+
+	// Introduce users to Marv (This takes 9 seconds)
+	animateIntroduction();
+	
+	// Brief users about what they can do with Marv (This takes 11 seconds)
+	setTimeout(function(){animateBrief();}, 9000);
+	
+	// Set click listenrs
+	setTimeout(function(){
+		document.getElementById('sign_in_button').onclick = function(){signIn();}
+	}, 10000);
 
 }
 
 /* Main thread */
 window.onload = function () {
 
-    /* Initialize Firebase */
-    firebase.initializeApp({
-        apiKey: "AIzaSyB2R3EKvah0v7TXIJhmE-0Fyp0z0cqwUws",
-        authDomain: "tgd-experiment.firebaseapp.com",
-        databaseURL: "https://tgd-experiment.firebaseio.com",
-        projectId: "tgd-experiment",
-        storageBucket: "tgd-experiment.appspot.com",
-        messagingSenderId: "262835571788",
-        appId: "1:262835571788:web:014ae0235c5278c8"
-    });
+	/* Initialize Firebase */
+	firebase.initializeApp({
+		apiKey: "AIzaSyB2R3EKvah0v7TXIJhmE-0Fyp0z0cqwUws",
+		authDomain: "tgd-experiment.firebaseapp.com",
+		databaseURL: "https://tgd-experiment.firebaseio.com",
+		projectId: "tgd-experiment",
+		storageBucket: "tgd-experiment.appspot.com",
+		messagingSenderId: "262835571788",
+		appId: "1:262835571788:web:014ae0235c5278c8"
+	});
 
-    // Register a Service Worker
-	if('serviceWorker' in navigator) {
-	    navigator.serviceWorker
-            .register('sw.js')
-            .then(function() { console.log("%cService Worker Registered!", "background:#222222; color:#BADA55;"); });
-	}
+	/* Set the language */
+	firebase.auth().languageCode = 'en';
 	
+	/* Initialize Firestore */
+	db = firebase.firestore();
+
+	/* Register a Service Worker */
+	if('serviceWorker' in navigator) navigator.serviceWorker.register('sw.js');
+
 	main();
 
 }
+
