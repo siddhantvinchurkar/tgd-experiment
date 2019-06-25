d746468be997d572e89ad71a87acdbfbf3180c49
Siddhant <siddhantvinchurkar@gmail.com>
2019-06-25 20:52:07 +0530

    .

--- sw.js
+++ sw.js
@@ -21,12 +21,6 @@ self.addEventListener('message', (event) => {
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
-
 workbox.routing.registerRoute(
   /\/images\//,
   new workbox.strategies.CacheFirst({
